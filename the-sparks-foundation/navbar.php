a0389ab80770e8ff95d9930019a057eaf6f9c5e3
mdhasancse <63066917+mdhasancse@users.noreply.github.com>
2021-10-09 11:44:41 +0530

    Add files via upload

---
+++ the-sparks-foundation/navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light" >
+      <a class="navbar-brand" href="index.php">THE STOCK INVESTOR BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color:#ff1455;">Home</a>
+              </li>
+              <li class="nav-item" >
+                <a class="nav-link" href="createuser.php" style="color:#ff1455;">Create User</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color:#ff1455;">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color:#ff1455;">Transaction History</a>
+              </li>
+          </div>
+       </nav>
